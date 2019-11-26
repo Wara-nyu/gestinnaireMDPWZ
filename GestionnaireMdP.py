543e840f7a6f2f6d4519828dfb15d6df65c3f4f5
Zakia <chabanezakia@gmail.com>
2019-11-26 15:22:46 +0100

    lecture du fichier

--- GestionnaireMdP.py
+++ GestionnaireMdP.py
@@ -2,12 +2,19 @@
 
 import csv
 
-with open('persons.csv', 'wr') as csvfile:
+with open('persons.csv', 'w') as csvfile:
+
 
     filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                           quotechar='|', quoting=csv.QUOTE_MINIMAL)
     filewriter.writerow(["URL", "Identifiant", "Mot de pass"])
     filewriter.writerow(["Amazon.com", "Florian", "Florianestfou"])
     filewriter.writerow(["facebook.com", "Flo", "jaimelaguitare" ])
     filewriter.writerow(["instagram", "Fl0R1an", "nem"])
- 
+
+with open('persons.csv', 'r') as csvfile: 
+    reader = csvfile.read()
+    print(reader)
+
+
+   
