9c4aa93e1184938fc315f5c8c1fa1e11c0011bea
Zakia <chabanezakia@gmail.com>
2019-11-26 14:31:08 +0100

    creation du fichier csv

--- GestionnaireMdP.py
+++ GestionnaireMdP.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
 
+import csv
 
+with open('persons.csv', 'wr') as csvfile:
+
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    filewriter.writerow(["URL", "Identifiant", "Mot de pass"])
+    filewriter.writerow(["Amazon.com", "Florian", "Florianestfou"])
+    filewriter.writerow(["facebook.com", "Flo", "jaimelaguitare" ])
+    filewriter.writerow(["instagram", "Fl0R1an", "nem"])
+ 
