4717e8b9fbe601e61b6d913843148138e984c346
Wara-nyu <w.bounkeua@gmail.com>
2019-11-27 19:08:15 +0100

    if elif else ne fonctionne pas

--- GestionnaireMdP.py
+++ GestionnaireMdP.py
@@ -1,7 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import csv
+start = input("recherche/entrer : ")
 
+<<<<<<< HEAD
+if start == entrer :
+    URL = input("entrez le site web : ")
+    Id = input("entrez votre identifiant : ")
+    mdp = input("entrez votre mot de passe : ")
+
+    with open('personal', 'a') as personal:
+        personal.write(URL + ", " + Id + ", " + mdp)
+
+elif start == recherche :
+    liste = input("nom du site : ")
+    with open("personal", "r") as fichier:
+        for ligne in fichier:
+            if liste in ligne :
+                print(ligne)
+else 
+    print("error")
+=======
 with open('persons.csv', 'w') as csvfile:
 
 
@@ -18,3 +36,4 @@ with open('persons.csv', 'r') as csvfile:
 
 
    
+>>>>>>> 543e840f7a6f2f6d4519828dfb15d6df65c3f4f5
