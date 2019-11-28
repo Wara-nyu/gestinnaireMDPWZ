878088ad90628244e91894ffd09a5b9a410e6c9f
Wara-nyu <w.bounkeua@gmail.com>
2019-11-28 16:27:47 +0100

    renomer le fichier

--- bidouillageW.py
+++ bidouillageW.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
-start = input("recherche/entrer : ")
-
-if start == entrer :
+start = input("Recherche/Entrer : ")
+start = start.capitalize()
+if start == "Entrer" :
     URL = input("entrez le site web : ")
     Id = input("entrez votre identifiant : ")
     mdp = input("entrez votre mot de passe : ")
-
+    URL = URL.capitalize()
     with open('personal', 'a') as personal:
-        personal.write(URL + ", " + Id + ", " + mdp)
+        personal.write(URL + ", " + Id + ", " + mdp + "\n")
 
-elif start == recherche :
+elif start == "Recherche" :
     liste = input("nom du site : ")
     with open("personal", "r") as fichier:
         for ligne in fichier:
             if liste in ligne :
                 print(ligne)
-else 
-    print("error")
+            else :
+                print("Désolé, je n'ai pas trouvé")
+else :
+    print ("Je n'ai pas compris votre requête")
